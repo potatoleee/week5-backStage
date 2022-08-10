b94be80a0690d556fad61c55de9a0c30495a97ee
李東穎 <dean19990226@gmail.com>
2022-08-10 18:40:22 +0800

    Update 2022-08-10T10:40:20.641Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,9 +2,26 @@
 
 $(function () {
   $('.btn__expand ').on('click', function (e) {
-    $('.btn__expand span ').toggleClass('open'); // $('.expand__content').slideToggle('slow');
+    $('.btn__expand span ').toggleClass('open');
+    $('.expand__content').slideToggle('slow'); // $('.expand__content').toggleClass('open');
+  }); // 還沒點選時是淺灰色
 
-    $('.expand__content').toggleClass('open');
+  var unSelected = "#6C757D"; // 被點選時是深灰色
+
+  var selected = "#212529";
+  $("#newAdminSelect").css("color", unSelected);
+  $("option").css("color", selected);
+  $("select").change(function () {
+    var selItem = $(this).val();
+
+    if (selItem == $(this).find('option:first').val()) {
+      $(this).css("color", unSelected);
+    } else {
+      $(this).css("color", selected);
+    }
+  });
+  ClassicEditor.create(document.querySelector('#editor'))["catch"](function (error) {
+    console.error(error);
   });
 });
 //# sourceMappingURL=all.js.map
