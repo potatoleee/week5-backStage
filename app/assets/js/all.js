0d36f3ba70c1a1ea70e298d2707e642adcbe7018
李東穎 <dean19990226@gmail.com>
2022-08-10 13:28:34 +0800

    edit modal finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,7 +2,8 @@ $(function() {
 
   $('.btn__expand ').on('click',function(e){
     $('.btn__expand span ').toggleClass('open');
-    $('.expand__content').slideToggle('slow');
+    // $('.expand__content').slideToggle('slow');
+    $('.expand__content').toggleClass('open');
 
   })
 
