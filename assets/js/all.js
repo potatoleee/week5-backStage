5fb3c1403562276ce6e173bd48bdec53914c1b77
李東穎 <dean19990226@gmail.com>
2022-08-10 13:28:58 +0800

    Update 2022-08-10T05:28:57.176Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,8 +2,9 @@
 
 $(function () {
   $('.btn__expand ').on('click', function (e) {
-    $('.btn__expand span ').toggleClass('open');
-    $('.expand__content').slideToggle('slow');
+    $('.btn__expand span ').toggleClass('open'); // $('.expand__content').slideToggle('slow');
+
+    $('.expand__content').toggleClass('open');
   });
 });
 //# sourceMappingURL=all.js.map
