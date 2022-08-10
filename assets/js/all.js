fc61c1d6494eb2aa5ba8054cd509fedf4676e318
李東穎 <dean19990226@gmail.com>
2022-08-10 11:32:18 +0800

    Update 2022-08-10T03:32:17.505Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,9 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  $('.btn__expand ').on('click', function (e) {
+    $('.btn__expand span ').toggleClass('open');
+    $('.expand__content').slideToggle('slow');
+  });
 });
 //# sourceMappingURL=all.js.map
